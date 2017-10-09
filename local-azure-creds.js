dd2f6b4302c50b3a51d39658423820080d61c3c5
Glen Mailer <glenjamin@gmail.com>
2017-10-09 16:04:55 +0100

    Get local azure credentials by shelling out instead of parsing JSON

--- local-azure-creds.js
+++ local-azure-creds.js
@@ -1,49 +1,23 @@
-const os = require('os');
-
-const PROFILE_PATH = '~/.azure/azureProfile.json';
-const TOKEN_PATH = '~/.azure/accessTokens.json';
+const exec = require('child_process').execSync;
 
 exports.loadLocalCredentials =
 function loadLocalCredentials(subscriptionId) {
-  const profile = loadAzureJson(PROFILE_PATH);
-  const subInfo = profile.subscriptions.find((s) => s.id == subscriptionId);
-  const accessTokens = loadAzureJson(TOKEN_PATH);
-  const access = extractAccessToken(accessTokens, subInfo);
+  try {
+    const tokenData = exec(`az account get-access-token -s '${subscriptionId}'`);
+    const data = JSON.parse(tokenData);
+    const authorization = `${data.tokenType} ${data.accessToken}`;
 
-  if (!access) {
+    return {
+      signRequest(webResource, callback) {
+        webResource.headers.authorization = authorization;
+        return callback(null);
+      }
+    };
+  } catch (ex) {
     throw new Error(`
-      Couldn't find valid azure credentials for ${subscriptionId}.
+      Couldn't get valid azure token for ${subscriptionId}.
       Use \`az login\` to populate local credential file first.
-      Tokens only last for 1 hour, but you can use \`az account get-access-token\` to force a refresh
+      Error: ${ex.message}
     `);
   }
-
-  return {
-    signRequest(webResource, callback) {
-      webResource.headers.authorization = access.authorization;
-      return callback(null);
-    }
-  };
-}
-
-function loadAzureJson(filename) {
-  const expanded = filename.replace('~', os.homedir());
-  try {
-    return require(expanded);
-  } catch (ex) {
-    throw new Error(`Failed to open ${filename}, log in with az cli first`);
-  }
-}
-
-function extractAccessToken(tokens, {tenantId}) {
-  const match = tokens.find((token) => (
-    String(token._authority).endsWith(tenantId)
-    && String(token.resource).includes("management")
-    && new Date(token.expiresOn) > new Date()
-  ));
-
-  return match && {
-    clientId: match._clientId,
-    authorization: `${match.tokenType} ${match.accessToken}`
-  };
 }
